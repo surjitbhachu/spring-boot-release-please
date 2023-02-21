3c03610f792d77483eb0a0b1c551c90ead36b5f0
Surjit Singh Bhachu <sbhachu@gmail.com>
2023-02-21 15:58:16 +0000

    feat: initial commit

---
+++ src/main/java/com/surj/springrelease/SpringReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.surj.springrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surj/springrelease/SpringReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surj.springrelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
